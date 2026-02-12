8da3f1778ebde248e01ff847c1209a35f22b8cae
OdnielT <odnielt28@gmail.com>
2026-02-12 21:49:58 +0000

    new test2 file

--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -1,10 +1,13 @@
 import styled from "styled-components";
+import Test2 from "./Test2";
 
 function Test() {
     return ( 
         <TestStyle>
             <h1>Hello London</h1>
             <h1>This is Odniel</h1>
+            <h1>Software Engineer</h1>
+            <Test2/>
         </TestStyle>
      );
 }
---
+++ src/Components/Test2.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+function Test2() {
+    return ( <Test2Style>
+        <h1>Hello Odniel</h1>
+    </Test2Style> );
+}
+
+const Test2Style = styled.div`
+  
+`;
+
+export default Test2;
