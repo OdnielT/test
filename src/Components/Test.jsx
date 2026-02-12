f6e85aa5e36dc99d254595e5367c42b75f064b93
OdnielT <odnielt28@gmail.com>
2026-02-12 17:57:28 +0000

    add test component

---
+++ src/Components/Test.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+function Test() {
+    return ( 
+        <TestStyle>
+            <h1>Hello London</h1>
+        </TestStyle>
+     );
+}
+
+const TestStyle = styled.div`
+  
+`;
+
+export default Test;
